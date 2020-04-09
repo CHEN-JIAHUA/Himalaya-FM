afdbdd2776270393c64292e4f396361a7869a1b8
CHAN-Edward <510984652@qq.com>
2020-04-09 14:59:25 +0800

    complete loading viewPage

---
+++ app/src/main/java/com/chenjiahua/himalayafm/view/LoadingView.java
@@ -0,0 +1,73 @@
+package com.chenjiahua.himalayafm.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+
+import com.chenjiahua.himalayafm.R;
+
+@SuppressLint("AppCompatCustomView")
+public class LoadingView extends ImageView {
+
+    //旋转的角度
+    private int rotateDegree = 0;
+
+    private boolean mNeedRotate = false;
+
+    public LoadingView(Context context) {
+        this(context,null);
+    }
+
+    public LoadingView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public LoadingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        //设置图片
+        setImageResource(R.mipmap.loading);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mNeedRotate = true;
+        //绑定到 window 的时候
+        post(new Runnable() {   //执行一个线程的内容
+            @Override
+            public void run() {
+                rotateDegree += 30;
+                rotateDegree = rotateDegree <= 360 ? rotateDegree : 0;
+                invalidate();
+                //判断是否还要继续旋转
+                if (mNeedRotate) {
+                    postDelayed(this,100);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        //从window 解绑
+        mNeedRotate = false;
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        /**
+         * rotate
+         * 第一个参数是旋转角度
+         * 第二个参数是旋转的x坐标
+         * 第三个参数是旋转的y坐标
+         */
+        canvas.rotate(rotateDegree,getWidth()/2,getHeight()/2);
+        super.onDraw(canvas);
+    }
+}
